8e134e4946da41694b1acbacc11a632cdfc48ad6
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-21 20:38:33 +0600

    java add answer1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let answer1 =prompt("Новый год на разных Языках");
+switch (answer1){
+    case "Кыргысча":
+    case "Кыргиском":
+    console.log("Жаны-Жыл");
+    break;
+    default:
+        console.log("Нет");
+        break;
+}

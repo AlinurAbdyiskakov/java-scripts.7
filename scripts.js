7cebcace9a8ac7057c0f95f068efe96aa405d020
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-21 20:54:50 +0600

    add answer2

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 //Example1
-let answer1 =prompt("Новый год на разных Языках");
+let answer1 = prompt("Новый год на разных Языках");
 switch (answer1){
     case "Кыргысча":
     case "Кыргиском":
@@ -13,3 +13,13 @@ switch (answer1){
         break;
 }
 //Example2
+let answer2 = prompt("Название Китая на Разных языках");
+switch (answer2){
+    case " English":
+        case "America":
+            console.log("Chine");
+            break;
+            case  "Казакыстан":
+                case  "Кыргызстан":  
+                console.log("Кытай");
+}

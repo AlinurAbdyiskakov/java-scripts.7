d6a64ba8f0c12ffcf620b954a28f4b92c1b831f0
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-21 20:43:16 +0600

    add the text new

--- scripts.js
+++ scripts.js
@@ -2,8 +2,11 @@ let answer1 =prompt("Новый год на разных Языках");
 switch (answer1){
     case "Кыргысча":
     case "Кыргиском":
-    console.log("Жаны-Жыл");
+    console.log("Жаны-Жыл!");
     break;
+    case "Рассийский":
+        case "Rashin":
+            console.log("Новый Год!");
     default:
         console.log("Нет");
         break;

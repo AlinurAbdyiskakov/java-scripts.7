da07a93428af5f8107f184bab5f42e998d03eb30
AlinurAbdyiskakov <abdyiskakovalinur7>
2021-01-22 16:41:52 +0600

    add answer3

--- scripts.js
+++ scripts.js
@@ -15,11 +15,43 @@ switch (answer1){
 //Example2
 let answer2 = prompt("Название Китая на Разных языках");
 switch (answer2){
-    case " English":
+    case " Рассийский":
         case "America":
             console.log("Chine");
             break;
-            case  "Казакыстан":
+            case  "Казакстан":
+                case  "Кыргызстан":  
+                console.log("Кытай");
+                default:
+                    console.log("Нет");
+                    break;
+}
+//Example3
+let answer3 = prompt("Назваеие Школ на разных языках!");
+switch (answer3){
+    case "Кыргысча":
+    case "Кыргиском":
+    console.log("Мектеп!");
+    break;
+    case "Рассийский":
+        case "Rashin":
+            console.log("школа!");
+            break;
+            case "English":
+                case "Англиский":
+                    console.log("School");
+    default:
+        console.log("Нет");
+        break;
+}
+//Example4
+let answer4 = prompt("Название каров в регионов Кыргызстане!");
+switch (answer4){
+    case " Ош":
+        case "Чуй":
+            console.log("сыйыр!");
+            break;
+            case  "":
                 case  "Кыргызстан":  
                 console.log("Кытай");
                 default:
